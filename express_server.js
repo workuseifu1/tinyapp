d8a3be39fa11198ac7c5f744492a03474b1ec339
workuseifu1 <workuseifu1@gmail.com>
2023-05-30 09:32:54 -0500

    Submit the form

--- express_server.js
+++ express_server.js
@@ -3,9 +3,10 @@ const app = express();
 const PORT = 8080; 
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
+  "b2xvn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 app.get("/urls", (req, res) => {
@@ -13,22 +14,20 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
-app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase };
+app.get("/urls/new", (req, res) => {  
+  res.render("urls_new");
+ });
+
+ app.post("/urls", (req,res) => {
+  console.log(req.body);
+  res.send("ok");
+ });
+
+app.get("/urls/:id", (req, res) => {  
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] }; 
   res.render("urls_show", templateVars);
 });
 
-
-// app.get("/", (req, res) => {
-//   res.send("Hello!");
-// });
-
-// app.get("/urls.json", (req, res) => {
-//   res.json(urlDatabase);
-// });
-// app.get("/hello", (req, res) => {
-//   res.send("<html><body>Hello <b>World</b></body></html>\n");
-// })
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
