8e7c81b5cf4e3bfba66e0f19316b77efd651fd4c
workuseifu1 <workuseifu1@gmail.com>
2023-05-29 22:49:03 -0500

    add secont rout and template

--- express_server.js
+++ express_server.js
@@ -13,6 +13,12 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase };
+  res.render("urls_show", templateVars);
+});
+
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
