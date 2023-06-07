d069843f50193d1145561529375fb872751336d1
workuseifu1 <workuseifu1@gmail.com>
2023-06-06 22:55:54 -0500

    User can only edit or delete their own URLs

--- express_server.js
+++ express_server.js
@@ -24,6 +24,7 @@ const userLookUp = function (email) {
 }
 
 
+
 // Middlewares API
 app.use(cookieParser())
 app.set("view engine", "ejs");
@@ -37,7 +38,7 @@ const urlDatabase = {
   i3BoGr: {
     longURL: "https://www.google.ca",
     userID: "aJ48lW",
-  },  
+  },
 };
 
 const users = {
@@ -52,14 +53,39 @@ const users = {
     password: "dishwasher-funk",
   },
 };
+//a function which returns URLs for where the userID is equal to the id of current loged in user
+const urlsForUser = function (id) {
+  
+  let urls = {};
+  for (let key in urlDatabase) {
+    if (urlDatabase[key].userID === id) {
+      urls[key] = { userID: id, longURL: urlDatabase[key].longURL };
+    }
+  }
+  return urls;
+};
 
-app.get("/urls", (req, res) => {  
-  const userRandomID = req.cookies['user_id'];  
+app.get("/", (req, res) => {
+  const user = req.body.id;
+  if (user) {
+    res.redirect("/urls");
+  }
+  res.redirect("login");
+});
+
+app.get("/urls", (req, res) => {
+  const userRandomID = req.cookies['user_id'];
+  const urlDatabase = urlsForUser(userRandomID)
+  
   const templateVars = {
     user: users[userRandomID],
-    user_id: req.cookies["user_id"],
+    userID: req.cookies["user_id"],
     urls: urlDatabase
   };
+
+  if (!userRandomID) {
+    res.status(400).send('<p>Please Login</p>');
+  }
   res.render("urls_index", templateVars);
 });
 //create new longURl by generation shortURL
@@ -70,38 +96,79 @@ app.get("/urls/new", (req, res) => {
     user_id: req.cookies["user_id"]
   }
   if (userRandomID) {
+    const templateVars = {
+      user: users[userRandomID],
+      user_id: req.cookies["user_id"]
+    };
     res.render("urls_new", templateVars);
   } else {
-    res.render("login", templateVars)
+    res.redirect("/login");
   }
+});
+app.get("/u/:shortURL", (req, res) => {
+  let shortURL = req.params.shortURL;
+  let longURL = urlDatabase[shortURL].longURL;
+  if (!longURL) {
+    res.status(403).send("No URL with that ID exists.")
+    } else {
+  res.redirect(longURL);
+ }
 });
 // handle new longURl by generation shortURL
 app.post("/urls", (req, res) => {
+  const shortUrl = generateRandomString();
   const userRandomID = req.cookies['user_id'];
-  if (!userRandomID) {
-    return res.send('<p>You must login first </p>')
-  } else {
-    const shortUrl = generateRandomString();
   const longURL = req.body.longURL;
-  urlDatabase[shortUrl] = {longURL,userID:userRandomID};
-
-  res.redirect(`/urls/${shortUrl}`);
-  }
   
+  
+  let newObj = { longURL: longURL, userID:  userRandomID };
+
+  if (!userRandomID) {
+    return res.status(403).send('<p>You must login first </p>');
+  }
+  if (longURL) {
+    
+    urlDatabase[shortUrl] = newObj;
+    
+    res.redirect(`/urls/${shortUrl}`);
+  } else {
+    res.status(403).send('<p>Invalid</p>');
+  }
 });
 
 app.get("/urls/:id", (req, res) => {
   const userRandomID = req.cookies['user_id'];
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id].longURL, user: users[userRandomID] };
-  let key = urlDatabase[req.params.id];
-  if (!key) {
-    return res.send(`<p>Id  by ${templateVars.id} is not in the database </p>`)
+  const shortURL = req.params.id;
+  let key = urlDatabase[shortURL];
+  if (urlsForUser(userRandomID) === {}) {
+    return res.status(403).send('<p>Please login</p>')
   }
+  if (!key) {
+    return res.status(404).send(`<p>No URl with that Id exists </p>`)
+  }
+  const templateVars = {
+    shortURL,
+    longURL: urlDatabase[shortURL].longURL,
+    user: users[userRandomID]
+  };
   return res.render("urls_show", templateVars);
 });
 // remove existing url
 app.post("/urls/:id/delete", (req, res) => {
   const id = req.params.id;
+  
+
+  const url = urlDatabase[id];
+  const userRandomID = req.cookies['user_id'];
+  const user = users[userRandomID];
+  if (!user) {
+    return res.status(403).send('<p> Request to delete URL denied</p>');
+  }
+  console.log(urlDatabase)
+  console.log(url)
+  if (userRandomID !== urlDatabase[id].userID) {
+    return res.status(403).send('<p>Request to delete URL denied</p>')
+  }
   delete urlDatabase[id];
   res.redirect("/urls");
 });
@@ -109,9 +176,15 @@ app.post("/urls/:id/delete", (req, res) => {
 app.post("/urls/:id", (req, res) => {
   const shortUrl = req.params.id;
   const userRandomID = req.cookies['user_id'];
-  const {longURL} = req.body;
-  urlDatabase[shortUrl] = {longURL,userID:userRandomID};
-  res.redirect(`/urls`);
+  const  longURL  = req.body.longURL;
+  if (urlsForUser("user_id") === {}) {
+    res.redirect("/urls");
+  } else if (urlDatabase[shortUrl].userID === urlsForUser("user_id")) {
+    urlDatabase[shortUrl].longURL = longURL;
+    res.redirect("/urls/");
+  } else {
+    res.redirect("/urls");
+  }
 });
 
 //summit login form and assign cookie value to user_id
@@ -119,7 +192,7 @@ app.post("/login", (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
   if (!email || !password) {
-    res.status(400).end('<p>You must provide an email and a pass </p>')
+    res.status(400).end('<p>You must provide an email and a passWord </p>')
   }
   let userID = userLookUp(email);
   if (userID === null) {
@@ -165,7 +238,7 @@ app.post("/register", (req, res) => {
     email: email,
     password: password
   };
-  console.log(users);
+  
   res.cookie("user_id", userRandpmID);
   res.redirect("/urls")
 });
